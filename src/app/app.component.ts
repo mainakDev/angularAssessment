2b213c9fadb47f4fbf397f5c231997a9783d5374
Mainak Dev <mainakonandroid@gmail.com>
2023-04-28 01:13:37 +0530

    angular assessment initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angularAssessment';
+export class AppComponent implements OnInit{
+  enteredText: string = '';
+  myControl: FormControl = new FormControl();
+
+  options = [
+    'Gina Williams',
+    'Jake Williams',
+    'Jamie John',
+    'John Doe',
+    'Jeff Stewart',
+    'Paula M. Keith',
+  ];
+
+  filteredOptions: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((val) => (val.length >= 1  && val[0] === '@' ? this.filter(val.substring(1)) : []))
+    );
+  }
+
+  filter(val: string): string[] {
+    return this.options.filter(
+      (option) => option.toLowerCase().indexOf(val.toLowerCase()) === 0
+    );
+  }
+
+  onFormReset(){
+    console.log(this.enteredText);
+    this.enteredText = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete'; 
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +15,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
